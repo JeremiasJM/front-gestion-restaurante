e99834b4518ff6d8704a8f64e23e4f005b3bbed8
georginacostilla <georginacostilla.gc@gmail.com>
2024-04-02 03:59:33 -0300

    se realizan actualizaciones en el contenido y en el diseño

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,4 +1,4 @@
-import Carousel from 'react-bootstrap/Carousel';
+import { Carousel, Button, Card } from 'react-bootstrap';
 import flyer1 from '../../resource/media/img-carousel/flyer-principal.png';
 import flyer2 from '../../resource/media/img-carousel/flyer-secundario.png';
 
@@ -22,12 +22,26 @@ const Home = () => {
         </Carousel.Item>
       </Carousel>
 
+      <Card className="text-center titulo" style={{
+        width: '75%', margin: '5% auto',
+        border: 'none',
+        backgroundColor: '#efe7df'
+      }}>
+        <Card.Header style={{
+          backgroundColor: '#efe7df'
+        }}>Bienvenidos</Card.Header>
+        <Card.Body className='parrafo'>
+          <Card.Text>
+          Te esperamos para vivir una experiencia inigualable! Donde la comida exquisita, los tragos de calidad premium y el 
+          ambiente amigable se combinan para hacer de cada visita un momento inolvidable. Nuestro equipo de bartenders expertos 
+          está listo para prepararte tus cócteles preferidos, mientras disfrutas de la mejor música en vivo o de nuestros eventos 
+          temáticos.
+          </Card.Text>
+          <Button variant="primary" style={{marginTop: '2%'}}>Hace tu reserva aquí</Button>
+        </Card.Body>
+      </Card>
     </>
   )
 }
 
 export default Home
-
-
-
-
