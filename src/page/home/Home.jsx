b8820bd2b67279b98b8c0ce1d08408bf9b2e9cce
georginacostilla <georginacostilla.gc@gmail.com>
2024-04-05 03:13:33 -0300

    se mejora el diseño del boton para realizar la reserva

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,6 +1,7 @@
-import { Carousel, Button, Card } from 'react-bootstrap';
-import flyer1 from '../../resource/media/img-carousel/flyer-principal.png';
-import flyer2 from '../../resource/media/img-carousel/flyer-secundario.png';
+import { Carousel, Card } from 'react-bootstrap';
+import flyer1 from '../../assets/resource/media/img-carousel/flyer-principal.png';
+import flyer2 from '../../assets/resource/media/img-carousel/flyer-secundario.png';
+import './home.css';
 
 const Home = () => {
   return (
@@ -32,12 +33,12 @@ const Home = () => {
         }}>Bienvenidos</Card.Header>
         <Card.Body className='parrafo'>
           <Card.Text>
-          Te esperamos para vivir una experiencia inigualable! Donde la comida exquisita, los tragos de calidad premium y el 
-          ambiente amigable se combinan para hacer de cada visita un momento inolvidable. Nuestro equipo de bartenders expertos 
-          está listo para prepararte tus cócteles preferidos, mientras disfrutas de la mejor música en vivo o de nuestros eventos 
-          temáticos.
+            Te esperamos para vivir una experiencia inigualable! Donde la comida exquisita, los tragos de calidad premium y el
+            ambiente amigable se combinan para hacer de cada visita un momento inolvidable. Nuestro equipo de bartenders expertos
+            está listo para prepararte tus cócteles preferidos, mientras disfrutas de la mejor música en vivo o de nuestros eventos
+            temáticos.
           </Card.Text>
-          <Button variant="primary" style={{marginTop: '2%'}}>Hace tu reserva aquí</Button>
+          <button className="button type1"></button>
         </Card.Body>
       </Card>
     </>
